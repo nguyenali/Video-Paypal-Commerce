ebef56abfc68ca4817354b67b32ce5d02acf7d48
Ali Nguyen <alin57717@gmail.com>
2020-10-31 20:36:10 -0700

    add in homepage template engine

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
