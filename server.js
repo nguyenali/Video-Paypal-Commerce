4398b32ed22028e9681e6cffabbd2c9ba106a933
Ali Nguyen <alin57717@gmail.com>
2020-11-01 22:13:02 -0800

    correct template engine to display html on server

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
+//Loads the express module
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const routes = require('./controllers/');
+const express = require('express');
+// const routes = require('./controllers/');
+const { read } = require('fs');
 
+//Creates our express server
+const app = express();
+const port = 3000;
 
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
 
+//Sets our app to use the handlebars engine
+app.set('view engine', 'hbs');
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
+//Sets handlebars configurations (we will go through them later on)
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    //new configuration parameter
+    extname: 'hbs'
+    }));
+
+//Serves static files (we need it to import a css file)
+app.use(express.static('public'))
+//Sets a basic route
+
+app.get('/', (req, res) => {
+    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('homepage', {layout : 'main'});
+    });
+
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port ${port}`));
