77997756306701d75c0be517bf825cf7cc9242c3
Ali Nguyen <alin57717@gmail.com>
2020-11-04 21:25:30 -0800

    correct sequelize function and session key

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,14 @@ if (process.env.NODE_ENV != "production") {
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize( 'database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
