6857414174b416e522f6a74605b489858734894c
Ali Nguyen <alin57717@gmail.com>
2020-11-08 21:29:01 -0800

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,9 @@ if (process.env.NODE_ENV != "production") {
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-
-  if(process.env.JAWSDB_URL) {
-    connection =mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-    connection =mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'alithanhhuynguyen'
-      database: 'stock_inventory_db'
-    })
-  }
+  
+  process.env.JAWSDB_URL,
+  
 );
 
 module.exports = sequelize;
